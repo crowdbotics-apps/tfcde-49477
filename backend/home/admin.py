a5efdda3eeac2932239a2e63660baa81944123e3
crowdbotics-apps <team@crowdbotics.com>
2024-09-25 14:03:24 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Jcdfer
+
+admin.site.register(Jcdfer)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Jcdfer(models.Model):
+    "Generated Model"
+    nbgh = models.BigIntegerField()
+    rivn = models.BigIntegerField()
